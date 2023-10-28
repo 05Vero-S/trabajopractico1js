1a234bf0876947d37d66b39c07f569282dc39745
Vero <vdsoria05@gmail.com>
2023-10-28 16:00:59 -0300

    agregue ejercicio 2 declarando 2 variables de texto y realice concatenacion de dichas variables y muestra en consola

---
+++ js/ejercicio2.js
@@ -0,0 +1,10 @@
+//Concatenacion de Cadenas
+
+//Declaro 2 variables
+const nombre = prompt("Ingrese un nombre: ");
+console.log(nombre);
+const apellido = prompt("Ingrese un apellido: ");
+console.log(apellido);
+
+const nombreCompleto = nombre + " " + apellido;
+console.log(nombreCompleto);
