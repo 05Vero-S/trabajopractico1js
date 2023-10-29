68b992f08787be868123b2f6bc64db0053875116
Vero <vdsoria05@gmail.com>
2023-10-29 17:09:55 -0300

    agregue ejercicio 11 del tp. usando condicionales y mostrando en pantalla con document.write

--- js/tp11.js
+++ js/tp11.js
@@ -3,4 +3,20 @@
 const numero= parseInt(prompt("Ingrese un número"));
 console.log(numero);
 
+if(numero % 2 == 0){
+    document.write(`El ${numero} es divisible por 2 <br>`);
+}
 
+if(numero % 3 == 0){
+    document.write(`El ${numero} es divisible por 3 <br>`);
+    }
+if(numero % 5 == 0){
+        document.write(`El ${numero} es divisible por 5 <br>`);
+    }
+if(numero % 7 == 0){
+        document.write(`El ${numero} es divisible por 7 <br>`);
+     }
+        
+    
+    
+    
